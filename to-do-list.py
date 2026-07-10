050a440d097b1af007471d53a2687d3943acacc3
yunchan-an-dev <yunchan0079@gmail.com>
2026-07-10 22:34:36 +0200

    Initial commit: erste Programmstruktur

---
+++ to-do-list.py
@@ -0,0 +1,11 @@
+# ----- import libraries -----
+import os   
+
+#------- define functions -----
+class ToDoList:
+
+
+#-------main function-------
+if __name__ == "__main__":
+todolist = ToDoList()
+todolist.window.mainloop()
