c5ce78d2f6480db67f53022a2bb677d15e31f4b5
yunchan-an-dev <yunchan0079@gmail.com>
2026-07-11 22:45:06 +0200

    Initial Commit: grobe Programmstruktur aktualisiert

--- to-do-list.py
+++ to-do-list.py
@@ -1,8 +1,23 @@
 # ----- import libraries -----
 import os   
+import json #für das Speichern und Laden der To-Do-Liste
+from tkinter import Tk, Label, Button, StringVar
+from tkinter import ttk
 
 #------- define functions -----
 class ToDoList:
+    def __init__(self): # Konstruktor
+        # Fenstertitel, Fenstergröße definieren
+        # Startvariablen initialisieren: self.text_line = StringVar()
+        # usw
+
+        #self. create_ui()
+
+    #def create_ui(self)
+    # hier wird die Benutzeroberfläche erstellt, z.B. Labels, Buttons, Eingabefelder usw.
+    # self.window = Tk()
+    # self.window.title("To-Do List")
+
 
 
 #-------main function-------
